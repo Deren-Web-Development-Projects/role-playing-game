4eca82b1b5daf116eea85fd5e2e3333ccf564595
Deren B <dbozerprogramming@gmail.com>
2022-08-16 16:13:18 -0400

    Project Creation

---
+++ scripts.js
@@ -0,0 +1,37 @@
+
+const hero = {
+    id: "hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceRoll: 6
+}
+
+const monster = {
+    id: "monster",
+    name: "Orc",
+    avatar: "images/orc.jpg",
+    health: 10,
+    diceRoll: 4
+}
+
+function renderCharacter(data) {
+    const {id, name, avatar, health, diceRoll} = data;
+
+    document.getElementById(id).innerHTML = `
+        <div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <p class="health">health: <b> ${health} </b></p>
+            <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
+        </div>   
+    `
+}
+
+// Create the Hero card
+renderCharacter(hero);
+
+// Create the monster card
+renderCharacter(monster);
+
+console.log('test');
